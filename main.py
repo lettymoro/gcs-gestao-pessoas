3ff6944b88bd9c4b3ab8235be48a5bcb9e525b75
lettymoro <lmoro@alunos.utfpr.edu.br>
2026-05-26 08:47:35 -0300

    Adiciona a funcionalidade de remover uma pessoa

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ def buscar():
     termo = input("Digite o nome que deseja buscar: ")
     encontrado = False
     for indice, pessoa in enumerate(pessoas):
-        # Transforma tudo em letra minúscula para facilitar a busca
         if termo.lower() in pessoa['nome'].lower():
             print(f"Encontrado -> [{indice}] Nome: {pessoa['nome']}")
             encontrado = True
@@ -29,13 +28,26 @@ def atualizar():
     listar()
     if len(pessoas) > 0:
         indice_str = input("Digite o número (entre colchetes) da pessoa que deseja atualizar: ")
-        
         if indice_str.isdigit():
             indice = int(indice_str)
             if 0 <= indice < len(pessoas):
                 novo_nome = input("Digite o novo nome: ")
                 pessoas[indice]['nome'] = novo_nome
                 print("Nome atualizado com sucesso!")
+            else:
+                print("Número não encontrado na lista.")
+        else:
+            print("Por favor, digite apenas números.")
+
+def remover():
+    listar()
+    if len(pessoas) > 0:
+        indice_str = input("Digite o número (entre colchetes) da pessoa que deseja remover: ")
+        if indice_str.isdigit():
+            indice = int(indice_str)
+            if 0 <= indice < len(pessoas):
+                removida = pessoas.pop(indice)
+                print(f"A pessoa '{removida['nome']}' foi removida com sucesso!")
             else:
                 print("Número não encontrado na lista.")
         else:
@@ -48,7 +60,8 @@ def menu():
         print("2. Listar pessoas")
         print("3. Buscar pessoa")
         print("4. Atualizar pessoa")
-        print("5. Sair")
+        print("5. Remover pessoa")
+        print("6. Sair")
         
         opcao = input("Escolha uma opção: ")
         
@@ -61,6 +74,8 @@ def menu():
         elif opcao == '4':
             atualizar()
         elif opcao == '5':
+            remover()
+        elif opcao == '6':
             print("Encerrando...")
             break
         else:
