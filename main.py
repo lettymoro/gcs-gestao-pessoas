bf68a147dac8be326a1a412e7b4fc169f9d983f8
lettymoro <lmoro@alunos.utfpr.edu.br>
2026-05-26 08:40:14 -0300

    Adiciona a funcionalidade de buscar pessoas

--- main.py
+++ main.py
@@ -13,12 +13,25 @@ def listar():
         for indice, pessoa in enumerate(pessoas):
             print(f"[{indice}] Nome: {pessoa['nome']}")
 
+def buscar():
+    termo = input("Digite o nome que deseja buscar: ")
+    encontrado = False
+    for indice, pessoa in enumerate(pessoas):
+        # Transforma tudo em letra minúscula para facilitar a busca
+        if termo.lower() in pessoa['nome'].lower():
+            print(f"Encontrado -> [{indice}] Nome: {pessoa['nome']}")
+            encontrado = True
+            
+    if not encontrado:
+        print("Ninguém encontrado com esse nome.")
+
 def menu():
     while True:
         print("\n--- Sistema de Gestão de Pessoas ---")
         print("1. Cadastrar pessoa")
         print("2. Listar pessoas")
-        print("3. Sair")
+        print("3. Buscar pessoa")
+        print("4. Sair")
         
         opcao = input("Escolha uma opção: ")
         
@@ -27,6 +40,8 @@ def menu():
         elif opcao == '2':
             listar()
         elif opcao == '3':
+            buscar()
+        elif opcao == '4':
             print("Encerrando...")
             break
         else:
